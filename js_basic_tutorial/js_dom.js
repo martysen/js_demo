18e3430a78ea4e1685902ed13672b6f036ca4546
Amartya Sen <sen@oakland.edu>
2021-10-18 20:24:43 -0400

    incomplete js file for js-dom tutorial

--- js_basic_tutorial/js_dom.js
+++ js_basic_tutorial/js_dom.js
@@ -1,90 +1,12 @@
 
 /********************************************************************************************************** */
 
-// JS and the DOM 
+// JS and the DOM -- Will be covered little more in-depth in later lectures 
+// DOM tree structure: WINDOW --> DOCUMENT --> ....
+// you can do a console log of the window object to see what methods are there 
+//console.log(window);
+//PROG. NOTE: Alert for example is a part of the window object. So we write window.alert('msg');
+// However, since window is the root level of the DOM, even if we don;t include the obj name, its methods will still work: alert('works');
 
 
 // ELEMENT SELECTORS
-
-// Single Element Selectors
-console.log(document.getElementById('my-form'));
-console.log(document.querySelector('.container'));
-// Multiple Element Selectors
-console.log(document.querySelectorAll('.item'));
-console.log(document.getElementsByTagName('li'));
-console.log(document.getElementsByClassName('item'));
-
-const items = document.querySelectorAll('.item');
-items.forEach((item) => console.log(item));
-
-
-// MANIPULATING THE DOM
-const ul = document.querySelector('.items');
-// ul.remove();
-// ul.lastElementChild.remove();
-ul.firstElementChild.textContent = 'Hello';
-ul.children[1].innerText = 'Brad';
-ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
-
-const btn = document.querySelector('.btn');
-// btn.style.background = 'red';
-
-
-// EVENTS
-
-// Mouse Event
-btn.addEventListener('click', e => {
-  e.preventDefault();
-  console.log(e.target.className);
-  document.getElementById('my-form').style.background = '#ccc';
-  document.querySelector('body').classList.add('bg-dark');
-  ul.lastElementChild.innerHTML = '<h1>Changed</h1>';
-});
-
-// Keyboard Event
-const nameInput = document.querySelector('#name');
-nameInput.addEventListener('input', e => {
-  document.querySelector('.container').append(nameInput.value);
-});
-
-
-// USER FORM SCRIPT
-
-// Put DOM elements into variables
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
-const msg = document.querySelector('.msg');
-const userList = document.querySelector('#users');
-
-// Listen for form submit
-myForm.addEventListener('submit', onSubmit);
-
-function onSubmit(e) {
-  e.preventDefault();
-  
-  if(nameInput.value === '' || emailInput.value === '') {
-    // alert('Please enter all fields');
-    msg.classList.add('error');
-    msg.innerHTML = 'Please enter all fields';
-
-    // Remove error after 3 seconds
-    setTimeout(() => msg.remove(), 3000);
-  } else {
-    // Create new list item with user
-    const li = document.createElement('li');
-
-    // Add text node with input values
-    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
-
-    // Add HTML
-    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
-
-    // Append to ul
-    userList.appendChild(li);
-
-    // Clear fields
-    nameInput.value = '';
-    emailInput.value = '';
-  }
-}
