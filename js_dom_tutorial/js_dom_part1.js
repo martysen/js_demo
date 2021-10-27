dc784d1a6c9b3e9136a4212cfee32d2082b12d19
Amartya Sen <sen@oakland.edu>
2021-10-27 19:53:17 -0400

    added a bootstrap template for later use in the css directory

---
+++ js_dom_tutorial/js_dom_part1.js
@@ -0,0 +1,18 @@
+/*
+    This tutorial File covers the basic of using JS with the DOM API for client side JS
+    The objective of these code snippets is to understand how JS can be used to introduce interactivity to your websites.
+
+    Pre-requisite: Studentds are expected to know HTML, CSS, and the Document Object Model (DOM)
+
+    In this script, we will cover the following JS based DOM selector concepts: 
+
+    1. document.getElementById()
+    2. document.getElementsByClassName()
+    3. document.getElementsByTagName()
+    4. document.querySelector()
+    5. document.querySelectorAll()
+
+    These concepts fall into the technological category called Vanilla JS
+*/
+
+
